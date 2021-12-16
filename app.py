898a2ab514fe75ad0c38cd3c98c0415b1e676b1c
Phillip Gütschow <Phillip.Guetschow.priv@gmail.com>
2021-12-17 00:58:33 +0100

    added navbar with bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import time
 from flask import Flask, redirect, url_for, render_template, request, flash, session
 from flask.helpers import send_from_directory
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(hours=1)
 
 
 #####################
@@ -20,9 +24,28 @@ def page_forbidden(e):
     return render_template('error.html', h1="ERROR 403", h2="Page forbidden"), 403
 
 
+#####################
+#   Website routes
+#####################
+
 @app.route('/', methods=["POST","GET"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        if 'username' in request.form:
+            session['user'] = request.form['username']
+            return redirect(url_for('index'))
+    else:
+        return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    return redirect(url_for('index'))
+
+
+#####################
+#   Backend routes
+#####################
 
 @app.route('/assets/<path:path>')
 def send_assets(path):
